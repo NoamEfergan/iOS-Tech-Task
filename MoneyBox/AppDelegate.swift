bdf273769af5e437f653df71cd0d5ff7fdbcb2cc
Noam Efergan <nefergan@kitmanlabs.com>
2024-10-03 08:52:43 +0100

    Start from VC

--- MoneyBox/AppDelegate.swift
+++ MoneyBox/AppDelegate.swift
@@ -11,7 +11,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow? 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+      let navController = UINavigationController()
+      let loginVC = LoginViewController()
+      navController.pushViewController(loginVC, animated: false)
+      window = UIWindow(frame: UIScreen.main.bounds)
+      window?.rootViewController = navController
+      window?.makeKeyAndVisible()
+      return true
     }
 }
 
